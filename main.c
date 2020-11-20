b9bde9cdce32a6465fe9799281e466b3bb3a323a
soyoonahn <70745113+soyoonahn@users.noreply.github.com>
2020-11-20 14:32:02 +0900

    02

--- main.c
+++ main.c
@@ -3,55 +3,74 @@
 
 #define N 6
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void init_board(char board[][N]);
+int player_move(int player, char board[][N]);
+void print_board(char board[][N]);
 
-void init_board(char board[N][N]);
-int player_put(int player, char board[N][N]);
-void print_board(char board[N][N]);
-
-int main(int argc, char *argv[]) {
-	
+int main(void)
+{	
 	char board[N][N];
-	int quit = 0;
-	
+	int quit=0;
+
 	init_board(board);
-	print_board(board); // 보드를 화면에 출력
-	
+	do	{
+		print_board(board);
+		quit = player_move(0, board);
+		if (quit == 1) break;
+		print_board(board);
+		quit = player_move(1, board);
+		if (quit == 1) break;
+	} while(quit == 0);
 	return 0;
 }
 
-void init_board(char board[N][N])
+void init_board(char board[][N])
 {
 	int x, y;
-	for(x=0;x<N;x++)
-		for(y=0;y<N;y++) board[x][y]=' '; //2차원 배열을 ''로 초기화 
+	for(x=0; x<N; x++)
+		for(y=0; y<N; y++) board[x][y] = ' ';
+	
+	board [2][2] = 'O';
+	board [2][3] = 'X';
+	board [3][2] = 'X';
+	board [3][3] = 'O';
 }
 
-int player_put(int player, char board[N][N]) 
+
+int player_move(int player, char board[N][N])
 {
 	int x, y, done = 0;
-	while (done != 1) {
-	printf("put a new white othello: ");
-	scanf("%d %d", &x, &y);
-	/*if 
-	
-	else printf("");
-	else if printf("");*/
+	while(done != 1) {
+		printf("\nput a new white othello: ");  
+		scanf("%d %d",&x, &y);
+		if(x == -1 && y == -1)	return 1;
+		if(board[x][y] == ' ')	break;
+		else printf("invaild input!\n"); 
 	}
-	if (player == 0) board[x][y] = 'X';
+	if(player == 0)	board[x][y] = 'X';
+
 	else board[x][y] = '0';
 	return 0;
 }
 
 void print_board(char board[N][N])
 {
-	int i;
-	printf(" 0 1 2 3 4 5\n");
-	for (i=0;i<N;i++){
+	int i, j;
+	printf(" ");
+	for(i=0; i<N; i++)
+		printf(" %i", i);
+		
+		printf("\n");
+
+	for(i=0; i<N; i++){	
 		printf(" -------------\n");
-		printf("%d",i);
-		printf("| %c \n", board[N][0]);
+		printf("%i|", i); 
+		for(j=0; j<N; j++){	
+			printf("%c|", board[i][j]);
+			
 		}
-		printf(" -------------\n");
+		printf("\n");
 	}
-	
+printf(" -------------\n");
+}
+
