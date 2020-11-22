3ca1ec0b3b308b24f906944adfeed7d9161d6307
soyoonahn <70745113+soyoonahn@users.noreply.github.com>
2020-11-22 19:37:06 +0900

    04

--- main.c
+++ main.c
@@ -1,27 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define N 6
 
-void init_board(char board[][N]);
-int player_move(int player, char board[][N]);
-void print_board(char board[][N]);
+#define WHITE 0
+#define BLACK 0
+#define EMPTY 2
+
+int player;
+int isGameEnd = 0;
+
+char board[N][N];
+
+extern void init_board(char board[][N]);
+extern int player_move(int player, char board[][N]);
+extern void print_board(char board[][N]);
 
 int main(void)
 {	
-	char board[N][N];
-	int quit=0;
 
-	init_board(board);
-	do	{
-		print_board(board);
-		quit = player_move(0, board);
-		if (quit == 1) break;
-		print_board(board);
-		quit = player_move(1, board);
-		if (quit == 1) break;
-	} while(quit == 0);
-	
-	return 0;
-}
+int row;
+int col;
+int board_row;
+int board_col;
 
+init_board(); // 게임 초기화
+
+while(isGameEnd == 0)
+{
+	print_board(); //판 배치와 WHITE, BLACK 점수 출력
+	if (able_to_place == 0)
+	{
+		isGameEnd == 1; // 게임 종료
+		continue; // 두 player 모두 배치가 불가능하면 반복문 빠져나가야 함
+		
+		input_place(&board_row, &board_col);
+	}
+		if(over_board(board_row, board_col == 1))
+		{
+			if (able_to_flip(board_row, board_col))
+			{
+				put_othello(board_row, board_col);
+				
+				while(able_to_flip(board_row, board_col))
+					try_to_flip(board_row, board_col);
+					
+				print_flip_number();
+				change_turn();
+					
+			}
+			else print_invalid_flip();
+		}
+		else print_invalid_range();
+		 
+	 } 
+	 
+	 check_result();	
+ } 
 
