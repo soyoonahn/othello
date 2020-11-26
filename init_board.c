9ce0230a432bc8bf0d39993fea6aaaf4a15b7671
soyoonahn <70745113+soyoonahn@users.noreply.github.com>
2020-11-26 22:48:58 +0900

    16

--- init_board.c
+++ init_board.c
@@ -14,13 +14,15 @@ void init_board(char board[][N])
 	int x, y;
 	for(x=0; x<N; x++)
 		for(y=0; y<N; y++) board[x][y] = ' '; //board [x][y]에 모두 공백을 출력  
-	
-	board [N/2-1][N/2-1] = 'O'; //초기 white와 black의 배치 
+		
+	//초기 white와 black의 배치
+	board [N/2-1][N/2-1] = 'O';  
 	board [N/2-1][N/2] = 'X';
 	board [N/2][N/2-1] = 'X';
 	board [N/2][N/2] = 'O';
 	
-	score[WHITE] = 2; //초기 white와 black의 점수 
+	//초기 white와 black의 점수
+	score[WHITE] = 2;  
 	score[BLACK] = 2;
 }
 
--- print_board.c
+++ print_board.c
@@ -11,6 +11,7 @@ extern int WHITE, BLACK;
 
 void print_board(char board[N][N]) //보드의 현재 상태를 화면에 출력 
 {
+	//column 번호 출력 
 	int i, j;
 	printf(" ");
 	for(i=0; i<N; i++)
@@ -18,15 +19,16 @@ void print_board(char board[N][N]) //보드의 현재 상태를 화면에 출력
 		
 		printf("\n");
 
+	
 	for(i=0; i<N; i++){	
 		printf(" -------------\n");
-		printf("%i|", i); 
+		printf("%i|", i); //row 번호 출력
 		for(j=0; j<N; j++){	
-			printf("%c|", board[i][j]);
+			printf("%c|", board[i][j]); //board[i][j]에 해당하는 원소 출력 
 			
 		}
 		printf("\n");
 	}
 printf(" -------------\n\n");
 printf(" STATUS - WHITE: %d, BLACK: %d\n", score[WHITE], score[BLACK]);
-}
+} //white와 black의 현재 score 출력 
