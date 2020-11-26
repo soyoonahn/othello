645bef6a2228a5ca5052b92ff2b7f211cb03dae0
soyoonahn <70745113+soyoonahn@users.noreply.github.com>
2020-11-26 19:42:09 +0900

    11

--- player_move.c
+++ player_move.c
@@ -16,26 +16,49 @@ int player_move(int player, char board[N][N])
 	int x, y, done = 0;
 	while(done != 1) {
 		if(player == 0){
-		
-		printf("\nput a new white othello: "); 
-		scanf("%d %d",&x, &y);
+			printf("\nput a new white othello: "); 
+			scanf("%d %d",&x, &y);
 		//종료 조건 
-		if(board[x][y] == ' ')	
-			{board[x][y] = 'O';	
-			score[WHITE]++;
-			break;}
-		else printf("invalid input! (already occupied).\n"); }
+			if(board[x][y] == ' ')	{
+				if ((x<0)||(y<0)||(x>=N)||(y>=N)){ 
+					printf("invalid input! (invalid range). \n");
+					continue;}
+				else board[x][y] = 'O';	
+				score[WHITE]++;
+				break;}
+			
+			else {
+				if ((x<0)||(y<0)||(x>=N)||(y>=N)){ 
+					printf("invalid input! (invalid range)\n");
+					continue;}
+				else {
+				printf("invalid input! (already occupied)\n");
+					continue;} 
+				}	
+					}
 	
 
 	else {
 		printf("\nput a new black othello: "); 
-		scanf("%d %d",&x, &y);
+			scanf("%d %d",&x, &y);
 		//종료 조건 
-		if(board[x][y] == ' ')	
-			{board[x][y] = 'X';	
-			score[BLACK]++;
-			break;}
-		else printf("invalid input! (already occupied).\n"); }}
+			if(board[x][y] == ' ')	{
+				if ((x<0)||(y<0)||(x>=N)||(y>=N)){ 
+					printf("invalid input! (invalid range)\n");
+					continue;}
+				else board[x][y] = 'X';	
+				score[BLACK]++;
+				break;}
+			
+			else {
+				if ((x<0)||(y<0)||(x>=N)||(y>=N)){ 
+					printf("invalid input! (invalid range)\n");
+					continue;}
+				else {
+					printf("invalid input! (already occupied)\n");
+					continue;}
+				 }
+		
+		}
 	return 0;
-}
-
+}}
